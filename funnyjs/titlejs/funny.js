d9db39233347098e4623153bd66e4da80d68ba21
PoorBE <bao3260@126.com>
2020-07-12 12:01:39 +0800

    修改log

--- funnyjs/titlejs/funny.js
+++ funnyjs/titlejs/funny.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/PoorBE/images_b/img/logo.jpg");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/PoorBE/images_b/img/favicon.png");
          document.title = 'ヽ(●-`Д´-)ノ哎呀，我挂掉了';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/PoorBE/images_b/img/logo.jpg");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/PoorBE/images_b/img/favicon.png");
          document.title = 'ヾ(Ő∀Ő3)ノ好哦！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
